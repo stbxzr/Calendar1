d73c5b608057f803af24c5e4c78764e483449ba9
Julian Almendarez <ppalmendarez@yahoo.com>
2021-03-25 23:43:53 -0500

    third commit

--- Calendar/task/Develop/javascript.js
+++ Calendar/task/Develop/javascript.js
@@ -1,6 +1,6 @@
-let currentHour= moment().hour(); // find current hour in military
+var nMoment = moment().format('MMMM Do YYYY, h:mm:ss a');
+var display = $('#currentDay');
+display.text(nMoment);
 
-if ($(condition) === currentHour ) 
-{
-    
-}
+var unixFormat = moment.unix(1318781876).format("MMM Do, YYYY, hh:mm:ss");
+$(".container").text(unixFormat);
